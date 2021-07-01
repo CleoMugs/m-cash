760cb25f1222e19b54dd8f08db41f11691379bb9
CleoMugs <cleophas.mugeni@gmail.com>
2021-07-01 10:53:08 +0300

    first heroku commit

--- src/samples/keys.py
+++
@@ -1,9 +0,0 @@
-business_shortCode = "174379" #Lipa Na Mpesa Shortcode
-phone_number = "254708252968"
-lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
-consumer_key = "px7I6TZH0oqIpkKTLCih3RRGhXz3vk60"
-consumer_secret = "skZ8rILvPN0Uf9lq"
-shortcode = "600000" #600981  600999  600344  601431 600000 601431
-amount = "1"
-
-test_msisdn = "254708374149"
